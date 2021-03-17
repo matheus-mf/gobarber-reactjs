d09afaae1e51508abaf25869098426b29f22e2ac
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-17 09:12:12 -0300

    fix: hook creation to sing-out the user

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,7 +4,7 @@ import { Form } from '@unform/web';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
